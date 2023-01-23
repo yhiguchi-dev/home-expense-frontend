4d86780d3fdf13d3b4a9885871e24517006f3df3
Yuki Higuchi <yuki@yhiguchi.dev>
2023-01-23 23:58:56 +0900

    Update nextjs

--- nextjs/src/app/ui/dashboard/Dashboard.tsx
+++ nextjs/src/app/ui/dashboard/Dashboard.tsx
@@ -1,6 +1,8 @@
+import { Add } from "@mui/icons-material";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import MenuIcon from "@mui/icons-material/Menu";
 import NotificationsIcon from "@mui/icons-material/Notifications";
+import { Fab, Stack } from "@mui/material";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import Badge from "@mui/material/Badge";
 import Box from "@mui/material/Box";
@@ -12,15 +14,14 @@ import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
 import Link from "@mui/material/Link";
 import List from "@mui/material/List";
-import Paper from "@mui/material/Paper";
 import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
 
-import Deposits from "./Deposits";
+import MyTable from "@/app/ui/dashboard/MyTable";
+
 import { mainListItems, secondaryListItems } from "./listItems";
-import Orders from "./Orders";
 
 const Copyright = (props: { sx?: { pt: number } }): JSX.Element => {
   return (
@@ -170,38 +171,14 @@ const DashboardContent = (): JSX.Element => {
         >
           <Toolbar />
           <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              {/* Chart */}
-              <Grid item xs={12} md={8} lg={9}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: "flex",
-                    flexDirection: "column",
-                    height: 240,
-                  }}
-                ></Paper>
+            <Stack spacing={2}>
+              <MyTable />
+              <Grid container justifyContent="flex-end">
+                <Fab color="primary" aria-label="add">
+                  <Add />
+                </Fab>
               </Grid>
-              {/* Recent Deposits */}
-              <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: "flex",
-                    flexDirection: "column",
-                    height: 240,
-                  }}
-                >
-                  <Deposits />
-                </Paper>
-              </Grid>
-              {/* Recent Orders */}
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-                  <Orders />
-                </Paper>
-              </Grid>
-            </Grid>
+            </Stack>
             <Copyright sx={{ pt: 4 }} />
           </Container>
         </Box>
