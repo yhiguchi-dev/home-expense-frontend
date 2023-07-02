3ac80454b22252695f3ab052c6b3cf7e4da6e4cb
Yuki Higuchi <yuki@yhiguchi.dev>
2023-07-02 22:21:44 +0900

    chore: debug

--- nextjs/src/lib/env.ts
+++ nextjs/src/lib/env.ts
@@ -1,5 +1,6 @@
 const env = {
-  backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL ?? "",
+  // backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL ?? "",
+  backendUrl: "http://192.168.2.108:8080",
 };
 
 export default env;
