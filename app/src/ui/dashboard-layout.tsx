b89773fdf5bf4b55a21a17b8694e326c46bfcf6f
Yuki Higuchi <yuki@yhiguchi.dev>
2023-11-19 23:44:38 +0900

    feat: fix

--- app/src/ui/dashboard-layout.tsx
+++ app/src/ui/dashboard-layout.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement, ReactNode } from "react";
 
 import LinkComponent from "@/ui/link-component";
-import Box from "@/ui/parts/layout/Box";
 import SideBar from "@/ui/parts/side-bar";
 import TopBar from "@/ui/parts/top-bar";
 
@@ -29,10 +28,10 @@ const DashboardLayout = ({
   return (
     <div className={style.parent}>
       <SideBar linkComponent={LinkComponent} attributes={attributes} />
-      <Box width="100%" height="100%">
+      <div>
         <TopBar />
         <div className={style.expenseTableBackground}>{children}</div>
-      </Box>
+      </div>
     </div>
   );
 };
